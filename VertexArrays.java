79435cc18ab8a0f0c29e2edcd2c3907226378c45
Sempai <38811532+Squarellt@users.noreply.github.com>
2019-05-03 18:18:47 +0300

    Update VertexArrays.java

--- VertexArrays.java
+++ VertexArrays.java
@@ -39,7 +39,38 @@ public class VertexArrays {
         }
         bind(0);
     }
-
+    
+    public static void addTriangleEBO(String name, float[] vertex, boolean color, boolean texture){
+        int VAO = glGenVertexArrays();
+        int VBO = glGenBuffers();
+        int EBO = glGenBuffers();
+        int[] inc = {
+          0,1,3,
+          1,2,3
+        };
+        bind(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, vertex, GL_STATIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, inc, GL_STATIC_DRAW);
+        int stride;
+        if (color && texture) stride = 8; else if (color && !texture) stride = 6; else stride = 3;
+        if (vertex != null) {
+            glVertexAttribPointer(0, 3, GL_FLOAT, false, stride * Float.BYTES, 0);
+            glEnableVertexAttribArray(0);
+            if (color) {
+                glVertexAttribPointer(1, 3, GL_FLOAT, false, stride * Float.BYTES, 3 * Float.BYTES);
+                glEnableVertexAttribArray(1);
+            }
+            if (texture) {
+                glVertexAttribPointer(2, 2, GL_FLOAT, false, stride * Float.BYTES, 6 * Float.BYTES);
+                glEnableVertexAttribArray(2);
+            }
+            VAOs.put(name, VAO);
+        }
+        bind(0);
+    }    
+    
     public static void bindVAO(String s){
         bind(VAOs.get(s));
     }
